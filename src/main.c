a461d2a25a66ee7dcb07960fae81aaaa1bbf5781
Daniele Moser <dnlmsr0@gmail.com>
2020-12-01 12:32:56 +0100

    Added game of life rules

--- src/main.c
+++ src/main.c
@@ -39,7 +39,6 @@ int main(int argc, char *argv[])
   curs_set(0);
   cbreak();
   noecho();
-  clear();
   bool dots[maxX][maxY];
   for (int x = 0; x < maxX; ++x) {
     for (int y = 0; y < maxY; ++y) {
@@ -51,25 +50,33 @@ int main(int argc, char *argv[])
   dots[2][2]=true;
   dots[1][2]=true;
   dots[0][1]=true;
-
-  for (int x = 0; x < maxX; ++x) {
-    for (int y = 0; y < maxY; ++y) {
-      int found = 0;
-      for (int x1 = x-1; x1 <= x+1; ++x1) {
-        for (int y1 = y-1; y1 <= y+1; ++y1) {
-          if (x1 >= 0 && y1>=0 && x1 < maxX && y1 < maxY){
-            if (x1!=x || y1!=y){
-              if (dots[x1][y1])++found;
+  while(true){
+    clear();
+    bool newDots[maxX][maxY];
+    for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) newDots[x][y]=dots[x][y];
+    for (int x = 0; x < maxX; ++x) {
+      for (int y = 0; y < maxY; ++y) {
+        int found = 0;
+        for (int x1 = x-1; x1 <= x+1; ++x1) {
+          for (int y1 = y-1; y1 <= y+1; ++y1) {
+            if (x1 >= 0 && y1>=0 && x1 < maxX && y1 < maxY){
+              if (x1!=x || y1!=y){
+                if (dots[x1][y1])++found;
+              }
             }
           }
         }
+        if(dots[x][y]){
+          if(found<2 || found>3)newDots[x][y]=false;
+          mvaddch(y,x,found +'0');
+        } else if(found == 3) newDots[x][y]=true;
       }
-      if(dots[x][y])mvaddch(y,x,found +'0');
     }
-  }
+    for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) dots[x][y]=newDots[x][y];
 
-	refresh();
-	getch();
+    refresh();
+    getch();
+  }
 	endwin();
   return 0;
 }
