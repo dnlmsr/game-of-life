f4536884d17095881eb06d567448bb2d5be858bc
Daniele Moser <dnlmsr0@gmail.com>
2020-12-01 14:02:25 +0100

    Tidy code!

--- src/main.c
+++ src/main.c
@@ -32,51 +32,46 @@ int getIndex(point Point, int maxX){
 int main(int argc, char *argv[])
 {
   srand(time(NULL));
-  int maxX, maxY;
 
   initscr();
+
+  int maxX, maxY;
   getmaxyx(stdscr,maxY,maxX);
+
   curs_set(0);
   cbreak();
   noecho();
+
   bool dots[maxX][maxY];
-  for (int x = 0; x < maxX; ++x) {
-    for (int y = 0; y < maxY; ++y) {
-      dots[x][y]=0;
-    }
-  }
-  dots[2][0]=true;
-  dots[2][1]=true;
-  dots[2][2]=true;
-  dots[1][2]=true;
-  dots[0][1]=true;
+  for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) dots[x][y]=0;
+
   while(true){
     clear();
+
     bool newDots[maxX][maxY];
     for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) newDots[x][y]=dots[x][y];
-    for (int x = 0; x < maxX; ++x) {
-      for (int y = 0; y < maxY; ++y) {
+
+    for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) {
         int found = 0;
-        for (int x1 = x-1; x1 <= x+1; ++x1) {
-          for (int y1 = y-1; y1 <= y+1; ++y1) {
+        for (int x1 = x-1; x1 <= x+1; ++x1) for (int y1 = y-1; y1 <= y+1; ++y1) {
             if (x1 >= 0 && y1>=0 && x1 < maxX && y1 < maxY){
               if (x1!=x || y1!=y){
                 if (dots[x1][y1])++found;
               }
             }
           }
-        }
         if(dots[x][y]){
           if(found<2 || found>3)newDots[x][y]=false;
           mvaddch(y,x,found +'0');
         } else if(found == 3) newDots[x][y]=true;
       }
-    }
+
     for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) dots[x][y]=newDots[x][y];
 
     refresh();
     if(getch() == 'q') break;
   }
+
 	endwin();
   return 0;
 }
