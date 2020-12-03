f53424ff72e8d4669477e2ef0be05bcf7d97e969
Daniele Moser <dnlmsr0@gmail.com>
2020-12-03 18:26:42 +0100

    Changed variable names for better reading

--- src/main.c
+++ src/main.c
@@ -10,16 +10,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-int maxX, maxY;
+int MAX_X, MAX_Y;
 
-void initDots(bool dots[maxX][maxY], char filePath[]){
+void initUniverse(bool universe[MAX_X][MAX_Y], char filePath[]){
   FILE *fp;
   fp = fopen(filePath, "r");
   int i = 0;
   while (!feof(fp)){
-    char str[maxX];
+    char str[MAX_X];
     fscanf(fp,"%s\n",str);
-    for (int j = 0; j < (int)sizeof(str); ++j) if(str[j]=='X') dots[j][i]= true;
+    for (int j = 0; j < (int)sizeof(str); ++j) if(str[j]=='X') universe[j][i]= true;
     ++i;
   }
 }
@@ -28,38 +28,38 @@ int main(int argc, char *argv[])
 {
   initscr();
 
-  getmaxyx(stdscr,maxY,maxX);
+  getmaxyx(stdscr,MAX_Y,MAX_X);
 
   curs_set(0);
   cbreak();
   noecho();
 
-  bool dots[maxX][maxY];
-  for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) dots[x][y]=0;
-  initDots(dots, argv[1]);
+  bool universe[MAX_X][MAX_Y];
+  for (int x = 0; x < MAX_X; ++x) for (int y = 0; y < MAX_Y; ++y) universe[x][y]=0;
+  initUniverse(universe, argv[1]);
 
   while(true){
     clear();
 
-    bool newDots[maxX][maxY];
-    for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) newDots[x][y]=dots[x][y];
+    bool newUniverse[MAX_X][MAX_Y];
+    for (int x = 0; x < MAX_X; ++x) for (int y = 0; y < MAX_Y; ++y) newUniverse[x][y]=universe[x][y];
 
-    for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) {
+    for (int x = 0; x < MAX_X; ++x) for (int y = 0; y < MAX_Y; ++y) {
         int found = 0;
         for (int x1 = x-1; x1 <= x+1; ++x1) for (int y1 = y-1; y1 <= y+1; ++y1) {
-            if (x1 >= 0 && y1>=0 && x1 < maxX && y1 < maxY){
+            if (x1 >= 0 && y1>=0 && x1 < MAX_X && y1 < MAX_Y){
               if (x1!=x || y1!=y){
-                if (dots[x1][y1])++found;
+                if (universe[x1][y1])++found;
               }
             }
           }
-        if(dots[x][y]){
-          if(found<2 || found>3)newDots[x][y]=false;
+        if(universe[x][y]){
+          if(found<2 || found>3)newUniverse[x][y]=false;
           mvaddch(y,x,ACS_BLOCK);
-        } else if(found == 3) newDots[x][y]=true;
+        } else if(found == 3) newUniverse[x][y]=true;
       }
 
-    for (int x = 0; x < maxX; ++x) for (int y = 0; y < maxY; ++y) dots[x][y]=newDots[x][y];
+    for (int x = 0; x < MAX_X; ++x) for (int y = 0; y < MAX_Y; ++y) universe[x][y]=newUniverse[x][y];
 
     refresh();
     if(getch() == 'q') break;
